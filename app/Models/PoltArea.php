66c3a0d73cbe2e490abc859fb9666499051b4290
Arfansalmanramadhan <arfansalman9@gmail.com>
2024-09-10 13:23:06 +0700

    install slug

--- app/Models/PoltArea.php
+++ app/Models/PoltArea.php
@@ -2,20 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PoltArea extends Model
 {
     use HasFactory;
+    use Sluggable;
     protected $table = 'polt-area';
     protected $fillable = [
         'profil_id',
-        'nama_polt',
-        'jenis',
         'daerah',
+        "slug",
         'latitude',
         'longitude',
         'ukuran_port'
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'daerah'
+            ]
+        ];
+    }
 }
