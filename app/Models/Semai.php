a8ec0e2206d5f767cae3135b657434adfdc79bc7
Arfansalmanramadhan <arfansalman9@gmail.com>
2024-09-11 10:05:18 +0700

    membuat model polt A-D

---
+++ app/Models/Semai.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Semai extends Model
+{
+    use HasFactory;
+    protected $table = "semai";
+    protected $fillable = [
+        "polt-area_id",
+        'total_berat_basah',
+        'sample_berat_basah',
+        'total_berat_kering',
+        'sample_berat_basah',
+        'kandungan_karbon',
+        'co2',
+    ];
+    /**
+     * Get the poltarea that owns the poltarea
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function poltarea(): BelongsTo
+    {
+        return $this->belongsTo(PoltArea::class, 'polt-area_id');
+    }
+}
