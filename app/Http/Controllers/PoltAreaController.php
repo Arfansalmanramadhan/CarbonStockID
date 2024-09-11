a759a164f671ab8257d3db9f9fc737632af027f1
Arfansalmanramadhan <arfansalman9@gmail.com>
2024-09-11 13:40:24 +0700

    belum selesai

--- app/Http/Controllers/PoltAreaController.php
+++ app/Http/Controllers/PoltAreaController.php
@@ -19,7 +19,11 @@ class PoltAreaController extends Controller
      */
     public function index()
     {
-        //
+        $poltArea = PoltArea::with('daerah')->get();
+        return response()->json([
+            'message' => 'PoltAreas fetched successfully',
+            'data' => $poltArea
+        ], 200);
     }
 
     /**
@@ -38,7 +42,7 @@ class PoltAreaController extends Controller
         // Ambil profil dari user yang sedang login, misal melalui Auth
         $user = auth()->user();
         $profile = $user->profil; // Menganggap setiap user memiliki 1 profil terkait
-        
+
         // Validasi request
         $validatedData = $request->validate([
             'daerah' => 'required|string|max:255',
@@ -48,88 +52,25 @@ class PoltAreaController extends Controller
 
         try {
             // Membuat instance PoltArea baru
-            $poltAreaa = new PoltArea;
-            $poltAreaa->daerah = $validatedData['daerah'];
-            $poltAreaa->latitude = $validatedData['latitude'];
-            $poltAreaa->longitude = $validatedData['longitude'];
-            $poltAreaa->save();
+            $poltArea = PoltArea::create([
+                "profil_id" => $profile->id,
+                "daerah" => $validatedData['daerah'],
+                "latitude" => $validatedData['latitude'],
+                "longitude" => $validatedData['longitude']
+            ]);
 
             // Response berhasil
             return response()->json([
-                'message' => 'PoltArea created successfully',
-                'data' => $poltAreaa
+                'message' => 'PoltArea berhasil di buat',
+                'data' => $poltArea
             ], 201);
         } catch (\Exception $e) {
             // Response error
             return response()->json([
-                'message' => 'Failed to create PoltArea',
+                'message' => 'Gagal membuat PoltArea',
                 'error' => $e->getMessage()
             ], 500);
         }
-
-        // Tentukan jenis polt berdasarkan input jenis
-        // if (in_array($request->jenis, ['Semai', 'Serasah', 'Tumbuhan bawah', 'Tanah'])) {
-        //     $polt_a = Polt_a::create([
-        //         "polt-area_id" => $poltArea->id,  // Assign polt-area_id
-        //         "jenis" => $request->jenis
-        //     ]);
-        //     if ($request->jenis == 'Semai') {
-        //         $polt_a->jenis = "Semai";
-        //         $polt_a->save();
-        //     } elseif ($request->jenis == 'Serasah') {
-        //         $polt_a->jenis = "Serasah";
-        //         $polt_a->save();
-        //     } elseif ($request->jenis == 'Tumbuhan bawah') {
-        //         $polt_a->jenis = "Tumbuhan bawah";
-        //         $polt_a->save();
-        //     } elseif ($request->jenis == 'Tanah') {
-        //         $polt_a->jenis = "Tanah";
-        //         $polt_a->save();
-        //     }
-        //     return response()->json([
-        //         "pesan" => "Data polt A berhasil disimpan",
-        //         "data" => $polt_a
-        //     ]);
-        // } elseif ($request->jenis == 'Pancang') {
-        //     $polt_b = Polt_b::create([
-        //         "polt-area_id" => $poltArea->id,  // Assign polt-area_id
-        //         "jenis" => $request->jenis
-        //     ]);
-        //     $polt_b->jenis == "Pancang";
-        //     $polt_b->save();
-        //     return response()->json([
-        //         "pesan" => "Data polt B berhasil disimpan",
-        //         "data" => $polt_b
-        //     ]);
-        // } elseif ($request->jenis == 'Tiang') {
-        //     $polt_c = Polt_c::create([
-        //         "polt-area_id" => $poltArea->id,  // Assign polt-area_id
-        //         "jenis" => $request->jenis
-        //     ]);
-        //     $polt_c->jenis = "Tiang";
-        //     $polt_c->save();
-        //     return response()->json([
-        //         "pesan" => "Data polt C berhasil disimpan",
-        //         "data" => $polt_c
-        //     ]);
-        // } elseif ($request->jenis == 'Pohon') {
-        //     $polt_d = Polt_d::create([
-        //         "polt-area_id" => $poltArea->id,  // Assign polt-area_id
-        //         "jenis" => $request->jenis
-        //     ]);
-        //     $polt_d->jenis = "Pohon";
-        //     $polt_d->save();
-        //     return response()->json([
-        //         "pesan" => "Data polt D berhasil disimpan",
-        //         "data" => $polt_d
-        //     ]);
-        // } else {
-        //     return response()->json(['error' => 'Jenis tidak ada'], 400);
-        // }
-        // return response()->json([
-        //     'pesa' => "Data berhasil disimpan ",
-        //     "pesan" => $poltArea
-        // ], 200);
     }
 
     /**
